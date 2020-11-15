6dd24bd278836763aa96dd531b61722b4a91f394
Jeffrey <jeffrch3@uci.edu>
2020-11-14 18:34:48 -0800

    Fixed? 4 steps functions working on get move

--- StudentAI.py
+++ StudentAI.py
@@ -1,10 +1,18 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
+
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 
 import datetime
+import math
+
+exploreConst = 1.4
+simThreshold = 5
+defaultWins = 1
+defaultSims = 5
+defaultUCT = 1000
 
 class Node():
     def __init__(self, color, move, parent = None):
@@ -14,6 +22,12 @@ class Node():
         self.sims = 0
         self.children = []
         self.parent = parent
+
+
+    def UCT(self):
+        if self.parent is None:
+            return defaultUCT
+        return self.wins / self.sims + exploreConst * math.sqrt((math.log(self.parent.sims), self.sims))
 
 
     def getWinRate(self):
@@ -35,6 +49,18 @@ class Node():
     def upChild(self, child):
         self.children.append(child)
 
+
+    def getWins(self):
+        if self.sims < simThreshold:
+            return defaultWins
+        return self.wins
+
+
+
+    def getSims(self):
+        if self.sims < simThreshold:
+            return defaultSims
+        return self.sims
 """
 FROM discord
     - 3 second timer: 300-600 playouts (low at start, peaks towards ends)
@@ -65,14 +91,26 @@ class StudentAI():
     def flatten(self, ini_list) -> list:
         return sum(ini_list, [])
 
+
     def isTimeLeft(self):
-        if (self.start - datetime.datetime.now()).seconds < 15:
+        time = datetime.datetime.now()
+        if (time - self.start).seconds < 10: #TODO Change 15 seconds to something smaller when debugging
             return True
         return False
 
-    def chooseMove(self, node) -> Move: #returns Move if valid move is available, else None
-        #choose random move from node given
+
+    def chooseMove(self, node) -> Move: #Given a node with a potential leaf, return that leaf node if it's not already part of the official children
+        found = False
         moves = self.flatten(self.board.get_all_possible_moves(node.color))
+        for m in moves:
+            found = False
+            for n in node.children:
+                if not found and n.move == m:
+                    found = True
+            if not found:
+                return m
+
+        """
         if len(moves) != 0:
             # move is possible
             i = randint(0, len(moves) - 1)
@@ -83,60 +121,74 @@ class StudentAI():
             # move is not possible, just change the color w/o changing board
             self.color = self.opponent[self.color]
             return None
+        """
 
-    def select(self, moves) -> Node:
-        child = None
-        result = None
+    def select(self) -> Node: #REMINDER: moves is the flattened list of all available moves
+        maxNode = self.root
+        maxUct = -1
+        ptr = self.root
+        uct = None
+        found = False
 
-        #expand all children from ROOT only
-        if len(self.root.children) == 0: #TODO: update so if only has some children, gets all
+        while len(ptr.children) != 0: #Node is not a leaf node
+            moves = self.flatten(self.board.get_all_possible_moves(self.color))
             for m in moves:
-                child = self.root.children.append(Node(self.opponent[self.color], m, self.root))
-                result = self.simulate(child) #TODO check
-                self.backProp(result, child)
+                found = False
+                for c in ptr.children:
+                    if not found and m == c.move:
+                        uct = c.UCT()
+                        if uct > maxUct:
+                            maxUct = uct
+                            maxNode = c
+                        found = True
+                if not found:
+                    return ptr #Node is a leaf node, return parent to expand later
 
-        #go to a leaf node randomly TODO UCT/UCB instead
-        leaf = self.root
-        leafIndex = None
-        while len(leaf.children) != 0:
-            leafIndex = randint(0, len(moves)-1)
-            leaf = leaf.children[leafIndex]
-            self.board.make_move(leaf.move, self.color)
+            ptr = maxNode
             self.color = self.opponent[self.color]
-        return leaf
+            if maxNode.move != -1:
+                self.board.make_move(maxNode.move, self.color)
 
-    def expand(self, leaf) -> Node:
-        moveToMake = self.chooseMove(leaf)
+        # Node is leaf node
+        return ptr #Same thing as line 135
+
+
+    def expand(self, node) -> Node:
+        child = None
+        moveToMake = self.chooseMove(node)
         if moveToMake is None:
             #moveToMake didn't have options TODO
             pass
         else:
             #moveToMake was valid and returned a Move object and updated board
-            child = Node(self.opponent[leaf.color], moveToMake, leaf)
-            leaf.children.append(child)
+            child = Node(self.opponent[node.color], moveToMake, node)
+            node.children.append(child) #This line actually adds children to a node
         return child
+
 
     def simulate(self, child):
         players = {1: "B", 2: "W"}
         winner = None
         counter = 0
 
-        self.color = child.color #prob dont need this line but just in case
+        #self.color = child.color #prob dont need this line but just in case
         while self.board.is_win(players[self.color]) == 0:
             moves = self.flatten(self.board.get_all_possible_moves(self.color))
             if len(moves) != 0: #player has moves
                 i = randint(0, len(moves) - 1)
                 self.board.make_move(moves[i], self.color)
                 self.color = self.opponent[self.color]
-                ++counter
+                counter += 1
             else: #player doesnt have moves, but game hasn't ended yet
                 self.color = self.opponent[self.color]
 
         winner = self.board.is_win(players[self.color])
         while counter != 0:
             self.board.undo()
-            --counter
+            self.color = self.opponent[self.color]
+            counter -= 1
         return winner
+
 
     def backProp(self, result, child):
         while child is not None:
@@ -145,13 +197,13 @@ class StudentAI():
                 child.upWins()
             child = child.parent
 
+
     def MCTS(self, moves) -> Move:
         while (self.isTimeLeft()):
-            self.select(moves)
-            leaf= self.select(self.root)
-            child = self.expand(leaf) #TODO check if expand() returns None
-            result = self.simulate(child)
-            self.backProp(result, child)
+            parent = self.select()
+            expand = self.expand(parent) #TODO check if expand() returns None
+            result = self.simulate(expand)
+            self.backProp(result, expand)
 
 #TODO CHECK THIS
         bestChild = None
@@ -161,20 +213,29 @@ class StudentAI():
             if self.root.children[i].getWinRate() > bestWR:
                 bestWR = self.root.children[i].getWinRate()
                 bestChild = self.root.children[i]
-            ++i
+            i += 1
 
-        return bestChild
+        return bestChild.move
         #return the move in Actions(state) whose node has highest number of playouts highest w/s
 
     def get_move(self,move):
-        if len(move) != 0:                                          #If opponent made move, length = 1, else 0
-            self.board.make_move(move,self.opponent[self.color])    #Update board with opponent's move
+        if len(move) != 0:
+            self.board.make_move(move,self.opponent[self.color])
+            if self.root.parent is None: #len(self.root.children) == 0
+                self.root = Node(move, self.opponent[self.color])
+            else:
+                i = 0
+                while i != len(self.root.children):
+                    if self.root.children[i].move == move:
+                        break
+                    i += 1
+                self.root = self.root.children[i]
         else:
-            self.color = 1                                          #Opponent couldn't make a move, pass
+            self.color = 1
+            self.root.color = 1                                     #Opponent couldn't make a move, pass
 
-        # TODO added
         self.start = datetime.datetime.now()
-
+        """
         if self.root.move == -1:
             #no move was made previously (ie first player's turn) TODO check if opponent got stuck
             #do MCTS normally
@@ -183,33 +244,24 @@ class StudentAI():
             #update root to move just picked from opponent if move exists
             i = 0
             while i != len(self.root.children):
-                if self.root.children[i].move is move:
+                if self.root.children[i].move == move:
                     break
-                ++i
+                i += 1
             self.root = self.root.children[i]
-
+        """
         moves = self.flatten(self.board.get_all_possible_moves(self.color))
-        move = self.MCTS(self, moves)
+        move = self.MCTS(moves)
 
         # update root to move just picked from MCTS
         i = 0
         while i != len(self.root.children):
-            if self.root.children[i].move is move:
+            if self.root.children[i].move == move:
                 break
-            ++i
+            i += 1
         self.root = self.root.children[i]
 
+        self.board.make_move(move, self.color)
 
-        #Determine and make moves below (BROKEN)
-        #For every current node, make a new tree and do MC; after a move has been made, make a new tree for that node and so on
-
-        # if len(self.tree.children) == 0:                            #Leaf node, must expand
-        # moves = self.board.get_all_possible_moves(self.color)
-        # index = randint(0,len(moves)-1)                             #Indexes move from list with first coordinates as selected checker and second coordinates as selected move
-        # inner_index =  randint(0,len(moves[index])-1)               #...
-        # move = moves[index][inner_index]
-
-        self.board.make_move(move,self.color)
         return move
 
 """
@@ -221,5 +273,34 @@ A, B, C, D
 expand: first time: expand all children of R
 simulate: first time: choose one A-D using UCT/UCB formula: choose random move until termination:
 back: termination, go up children using parent pointer (while also Board.undo()), update w, s in each node
+"""
+
+
+
+# OLD SELECT TODO
+"""
+child = None
+result = None
+
+#expand all children from ROOT only
+
+if len(self.root.children) != len(moves): #TODO: update so if only has some children, gets all
+    for m in moves:
+        for n in self.root.children:
+            if moves
+            child = self.root.children.append(Node(self.opponent[self.color], m, self.root))
+            result = self.simulate(child) #TODO check
+            self.backProp(result, child)
+
+
+#go to a leaf node randomly TODO UCT/UCB instead
+leaf = self.root
+leafIndex = None
+while len(leaf.children) != 0:
+    leafIndex = randint(0, len(moves)-1)
+    leaf = leaf.children[leafIndex]
+    self.board.make_move(leaf.move, self.color)
+    self.color = self.opponent[self.color]
+return leaf
 """
 
