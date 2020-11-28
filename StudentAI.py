7792138c9ea6d8db656de73ee1e6c8cdc0276723
Isabelle Wang <ipwang@uci.edu>
2020-11-27 22:27:28 -0800

    invalid move error in select due to a player winning

--- StudentAI.py
+++ StudentAI.py
@@ -12,11 +12,12 @@ EXPLORE_CONSTANT = 2
 SIM_THRESHOLD = 3 #5
 DEFAULT_WIN = 9 #2
 DEFAULT_SIM = 10 #5
-DEFAULT_UCT = 1000
+# DEFAULT_UCT = 1000
 FEW_MOVES = 4
 SHORT_TURN = 7
-FULL_TURN = 12
-DEPTH_LEVEL = 10
+FULL_TURN = 10 #12
+DEPTH_LEVEL = 15 #10
+select_ctr = 0
 
 class Node():
     def __init__(self, color, move, parent=None):
@@ -29,8 +30,10 @@ class Node():
 
     def UCT(self):
         if self.parent is None:
-            return DEFAULT_UCT
-        return self.wins / self.sims + EXPLORE_CONSTANT * math.sqrt((math.log(self.parent.getSims()) / self.getSims()))
+            sp = 100
+        else:
+            sp = self.parent.getSims()
+        return self.getWins() / self.getSims() + EXPLORE_CONSTANT * math.sqrt(math.log(sp) / self.getSims())
 
     def getWinRate(self):
         if self.sims != 0:
@@ -54,6 +57,19 @@ class Node():
     def upSims(self):
         self.sims += 1
 
+'''
+def UCT(parent, child=None):
+    if parent is None:
+        sp = 100 # change to 1 or 2? see if that helps
+    else:
+        sp = parent.getSims()
+
+    if child is not None:
+        return child.getWins() / child.getSims() + EXPLORE_CONSTANT * math.sqrt(math.log(sp) / child.getSims())
+    else:
+        return DEFAULT_WIN / DEFAULT_SIM + EXPLORE_CONSTANT * math.sqrt(sp / DEFAULT_SIM)
+    # return self.wins / self.sims + EXPLORE_CONSTANT * math.sqrt((math.log(self.parent.getSims()) / self.getSims()))
+'''
 
 class StudentAI():
     def __init__(self, col, row, p):
@@ -79,10 +95,78 @@ class StudentAI():
         return False
 
     def select(self, moves) -> Node:
+        # TODO breaks when someone wins
+        # even if we can detect the win first, what do?
+            # backtrack: may not find child that is valid,
+            # may have to go up multiple generatsion,
+            # may waste time
+        '''
+        # does not need mvoes param
+        ptr = self.root
+        maxNodes = []
+
+        while len(ptr.children) != 0:
+            # ptr is not a leaf; it has some to all children/moves
+            moves = self.flatten(self.board.get_all_possible_moves(self.opponent[ptr.color]))
+            if len(ptr.children) < len(moves):
+                # ptr has only expanded some children/moves
+                return ptr
+
+            maxNodes.clear()
+            maxUct = -1
+            for c in ptr.children:
+                uct = c.UCT()
+                if uct > maxUct:
+                    maxUct = uct
+                    maxNodes.clear()
+                    maxNodes.append(c)
+                elif uct == maxUct:
+                    maxNodes.append(c)
+            i = randint(0, len(maxNodes) - 1)
+            maxNode = maxNodes[i]
+
+            self.board.make_move(maxNode.move, ptr.color)
+            ptr = maxNode
+
+        return ptr
+        '''
+
+        '''
+        # CODE SUBMITTED FOR MINIMAL AI; doesnt raise Index error in expand()
+        maxNode = self.root
+        maxUct = -1
+        ptr = self.root
+        uct = None
+        found = False
+        
+        while len(ptr.children) != 0:  # Node is not a leaf node
+            moves = self.flatten(self.board.get_all_possible_moves(self.color))
+            for m in moves:
+                found = False
+                for c in ptr.children:
+                    if not found and m == c.move:
+                        uct = c.UCT()
+                        if uct > maxUct:
+                            maxUct = uct
+                            maxNode = c
+                        found = True
+                if not found:
+                    return ptr  # Node is a leaf node, return parent to expand later
+            ptr = maxNode
+            self.color = self.opponent[self.color]
+            if maxNode.move != -1:
+                self.board.make_move(maxNode.move, self.color)
+        # Node is leaf node
+        return ptr  # Same thing as line 135
+        '''
+
+
+        # CODE FROM PRE-THANKSGIVING; raises Index error in expand()
         ptr = self.root
         maxNode = None
         maxNodes = []
         uct = None
+        select_ctr = 0
 
         while len(ptr.children) == len(moves):
             # all of ptr's children have been expanded
@@ -103,8 +187,18 @@ class StudentAI():
             maxNode = maxNodes[i]
 
             self.board.make_move(maxNode.move, ptr.color)
+            select_ctr += 1
+
             ptr = maxNode
             moves = self.flatten(self.board.get_all_possible_moves(self.opponent[ptr.color]))
+            players = {1: "B", 2: "W"}
+            if self.board.is_win(players[self.opponent[ptr.color]])!= 0:
+                # TODO invalid move error!!!
+                # someone won
+                # add and check attribute in Node class to see if it is terminal
+                # in MCTS: if is terminal, DO SMTH
+                return -1
+
             # if len(moves) == 0:
             #     # no moves possible
             #     # TODO alternatively: move up using parent pointers to a parent with 1+ child?
@@ -158,11 +252,11 @@ class StudentAI():
 
 
     def king_heuristic(self):
-        '''
-        gives a higher score to boards that are closer to attaining more kings
-        king piece: 100 points each
-        man piece: 5*row
-        '''
+        # gives a higher score to boards that are closer to attaining more kings
+        # TODO improve: change point values? calculate by specific pieces?
+        kings_worth = 100
+        mans_worth = 5
+
         score = 0
         players = {0: ".", 1: "B", 2: "W"}
         player = players[self.color]
@@ -177,15 +271,15 @@ class StudentAI():
                     #exists checker piece and color matches
                     if piece.is_king:
                         # piece is a king
-                        score += 100
+                        score += kings_worth
                     else:
                         # piece is a man
                         if self.color == 1:
                             # black
-                            score += r*5
+                            score += r*mans_worth
                         else:
                             # white
-                            score += 5*(board_row-r-1)
+                            score += mans_worth*(board_row-r-1)
         return score
 
 
@@ -195,7 +289,8 @@ class StudentAI():
         winner = None
         counter = 0
         depth = 0
-        while self.board.is_win(players[color]) == 0 or depth == DEPTH_LEVEL:
+
+        while self.board.is_win(players[color]) == 0 or depth <= DEPTH_LEVEL:
             moves = self.flatten(self.board.get_all_possible_moves(color))
             if len(moves) != 0:
                 # player has moves
@@ -237,12 +332,19 @@ class StudentAI():
 
     def backProp(self, result, child):
         counter = 0
+        # print("child.move: ", child.move)
+        # print("num real wins: ", child.wins)
+        # print("num real sims: ", child.sims)
         while child != self.root:
             child.upSims()
             if result != child.color:
                 child.upWins()
             child = child.parent
             counter += 1
+            # print("child.move: ", child.move)
+            # print("num real wins: ", child.wins)
+            # print("num real sims: ", child.sims)
+
         # child is the root
         child.upSims()
         if result != child.color:
@@ -259,9 +361,13 @@ class StudentAI():
 
         while (self.isTimeLeft()):
             parent = self.select(moves)
-            expand = self.expand(parent)  # TODO check if expand() returns None
-            result = self.simulate(expand)
-            self.backProp(result, expand)
+            if parent != -1:
+                expand = self.expand(parent)
+                result = self.simulate(expand)
+                self.backProp(result, expand)
+            else:
+                for i in range(select_ctr):
+                    self.board.undo()
 
         bestMove = None  # self.root.children[i].move
         if len(self.root.children) == 0:
@@ -317,7 +423,6 @@ class StudentAI():
         # print("player's turn: ", self.color)
         # print("len moves", len(moves))
         self.board.make_move(move, self.color)
-        # TODO remove these print statements for AI_Runner
         # print("len children", len(self.root.children))
         # print("num real wins at root: ", self.root.wins)
         # print("num real sims at root: ", self.root.sims)
